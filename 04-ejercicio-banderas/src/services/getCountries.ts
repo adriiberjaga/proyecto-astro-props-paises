f83403af416512e40f0f1805aab0fd37292d0de7
adriiberjaga <adriabdd@gmail.com>
2025-06-03 09:34:42 +0200

    .

--- 04-ejercicio-banderas/src/services/getCountries.ts
+++ 04-ejercicio-banderas/src/services/getCountries.ts
@@ -1,7 +1,7 @@
 import type { countryInterface } from '../types/countryInterface';
 
 export async function getCountries(): Promise<countryInterface[]> {
-  const URL = 'https://restcountries.com/v3.1/name/deutschland'
+  const URL = 'https://restcountries.com/v3.1/name/spain'
   // const URL = 'https://restcountries.com/v3.1/all'
 
   try {
